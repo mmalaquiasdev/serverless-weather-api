37551e6939ce304d2930169746acabc69c669dee
Mateus Malaquias <mateus.malaquias@globalhitss.com.br>
2018-08-07 11:37:50 -0300

    Fixing error response obj

--- src/helpers/response/responseJson.js
+++ src/helpers/response/responseJson.js
@@ -1,7 +1,7 @@
 const createResponseJSON = (statusCode, body) => ({
   statusCode,
   headers: { 'Content-Type': 'application/json; charset=utf-8' },
-  body: JSON.stringify(body),
+  body,
 });
 
 module.exports = createResponseJSON;
