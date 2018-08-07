696389a3e67f46b0d34ef06b37a8eb000bfe897e
Mateus Malaquias <mateus.malaquias@globalhitss.com.br>
2018-08-07 18:11:06 -0300

    Created response json fabric test

---
+++ src/helpers/response/__tests__/resonseJson.test.js
@@ -0,0 +1,11 @@
+const createResponseJSON = require('../responseJson');
+
+test('Shloud create a json object with statusCode, header values and a body with some data', () => {
+  const body = {
+    data: 'test',
+  };
+  const response = createResponseJSON(200, body);
+  expect(response).not.toBeUndefined;
+  expect(response.statusCode).toBe(200);
+  expect(response.body.data).toBe('test');
+});
