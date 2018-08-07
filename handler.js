5dabe9a8c6cb56ab7a4bec880cc43a299ec7aab1
Mateus Malaquias <mateus.malaquias@globalhitss.com.br>
2018-08-06 22:24:46 -0300

    Removed logs

--- handler.js
+++ handler.js
@@ -2,8 +2,7 @@ const { responseSucess } = require('./src/helpers/response');
 
 module.exports.about = (event, context, callback) => {
   console.log('init about lambda');
-  console.log('responseSucess', responseSucess);
   const sucess = responseSucess('Go Serverless v1.0! Your function executed successfully!', context.awsRequestId);
-  console.log('sucess', sucess);
+
   callback(null, sucess);
 };
